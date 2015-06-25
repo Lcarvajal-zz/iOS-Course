a0bab72da50772c72065e0e18c19b7f53be6b59d
Lukas Carvajal <lukascarvajal@gmail.com>
2015-06-25 02:39:58 -0400

    Initial Commit

---
+++ iOS-Course/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-Course
+//
+//  Created by Lukas Carvajal on 6/25/15.
+//  Copyright (c) 2015 Lukas Carvajal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-Course/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-Course
+//
+//  Created by Lukas Carvajal on 6/25/15.
+//  Copyright (c) 2015 Lukas Carvajal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
