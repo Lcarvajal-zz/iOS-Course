8403704031f2da3bfbfdcb87bbed29e5d33b8cc4
Lukas Carvajal <lukascarvajal@gmail.com>
2015-06-25 06:17:35 -0400

    complete user login

--- iOS-Course/LoginViewController.m
+++ iOS-Course/LoginViewController.m
@@ -10,6 +10,9 @@
 #import <Parse/Parse.h>
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailTF;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+- (IBAction)loginPressed:(id)sender;
 
 @end
 
@@ -25,4 +28,33 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)loginPressed:(id)sender {
+    
+    // convert email input into username
+    NSString* username = [self.emailTF.text lowercaseString];
+    username = [username stringByReplacingOccurrencesOfString:@"@my.fsu.edu" withString:@""];
+    
+    
+    [PFUser logInWithUsernameInBackground:username password:self.passwordTF.text block:^(PFUser *user, NSError *error) {
+        if (user) {
+            // user logs in
+            [self performSegueWithIdentifier:@"loginSegue" sender:self];
+        } else {
+            // error in login
+            NSString *errorString = [[error userInfo] objectForKey:@"error"];
+            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                                           message:errorString
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+            
+            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"Ok"
+                                                                    style:UIAlertActionStyleDefault
+                                                                  handler:^(UIAlertAction* action) {}];
+            // add action to alert and show to user
+            [alert addAction:defaultAction];
+            [self presentViewController:alert animated:YES completion:nil];
+
+        }
+    }];
+    
+}
 @end
